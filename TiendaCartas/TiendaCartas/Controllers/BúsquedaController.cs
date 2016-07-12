8894e9856dddcd53dfd53e5fcff9c4b898147159
Francisco <franciscoadiazh@gmail.com>
2016-07-12 18:13:14 -0400

    Trabajos de búsquedas

---
+++ TiendaCartas/TiendaCartas/Controllers/BúsquedaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaCartas.Controllers
+{
+    public class BúsquedaController : Controller
+    {
+        // GET: Búsqueda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
