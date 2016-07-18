be75139543682b1e8da0678388a66ce2cda3b944
Josue <josuesoto.co@gmail.com>
2016-07-18 00:34:58 -0400

    data

--- TiendaCartas/TiendaCartas/Models/IdentityModels.cs
+++ TiendaCartas/TiendaCartas/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace TiendaCartas.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("tiendacartasEntities", throwIfV1Schema: false)
         {
         }
 
