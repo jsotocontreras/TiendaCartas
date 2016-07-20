d1bd41f09266acec6336d49b1a33539f9edc52bb
Francisco <franciscoadiazh@gmail.com>
2016-07-20 18:42:06 -0400

    Sistema de Search

---
+++ TiendaCartas 2/TiendaCartas 2/Scripts/navegador.js
@@ -0,0 +1,15 @@
+﻿function openNav() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("Debe ingresar datos en el sistema de búsqueda");
+        return false
+    }
+    else
+        document.getElementById("mySidenav").style.width = "20%";
+    return false
+}
+
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
